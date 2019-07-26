82f6d501d6a41dc06bf3c721490ea49943f619b0
mupfpilou <41705573+mupfpilou@users.noreply.github.com>
2019-07-26 12:30:29 +0200

    Update menu.js

--- config/menu.js
+++ config/menu.js
@@ -1,7 +1,8 @@
 const menu = [
   { type: 'config', id: 'news' },
   { type: 'config', id: 'tournaments' },
-  { type: 'config', id: 'info' }
+  { type: 'config', id: 'info' },
+  { type: 'page', title: 'Animations', id: 4012, link: '/animations' }
 ]
 
 module.exports = menu
